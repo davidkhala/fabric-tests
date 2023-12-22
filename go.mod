bae61c283de0f84a7041ba2ab7cdab7271ebb2ca
david <david-khala@hotmail.com>
2023-12-22 09:13:57 +0800

    try to fix types

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/davidkhala/fabric-tests
+
+go 1.21
+
+require (
+	github.com/davidkhala/fabric-common/golang v0.0.0-20231221233639-b354f14c02bf
+	github.com/davidkhala/goutils v1.5.5
+	github.com/hyperledger-twgc/tape v0.2.6
+	github.com/hyperledger/fabric-protos-go-apiv2 v0.3.1
+	github.com/kortschak/utter v1.5.0
+	github.com/sirupsen/logrus v1.9.3
+	google.golang.org/grpc v1.60.1
+)
+
+require (
+	github.com/davidkhala/goutils/grpc v0.0.0 // indirect
+	github.com/davidkhala/protoutil v0.0.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/hyperledger/fabric v2.1.1+incompatible // indirect
+	github.com/hyperledger/fabric-protos-go v0.0.0-20220202165055-956c75de7b17 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ tape_sdk_test.go
@@ -0,0 +1,165 @@
+package tape
+
+import (
+	"context"
+	"fmt"
+	"github.com/davidkhala/fabric-common/golang"
+	"github.com/davidkhala/goutils"
+	"github.com/hyperledger-twgc/tape/pkg/infra/basic"
+	tapeEngine "github.com/hyperledger-twgc/tape/pkg/infra/trafficGenerator"
+	"github.com/hyperledger/fabric-protos-go-apiv2/common"
+	"github.com/hyperledger/fabric-protos-go-apiv2/orderer"
+	"github.com/hyperledger/fabric-protos-go-apiv2/peer"
+	"github.com/kortschak/utter"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"testing"
+)
+
+var logger = logrus.New()
+
+// peer0.icdd
+var peer0_icdd = golang.Node{
+	Addr:                  "localhost:8051",
+	TLSCARoot:             "/home/davidliu/delphi-fabric/config/ca-crypto-config/peerOrganizations/icdd/tlsca/tlsca.icdd-cert.pem",
+	SslTargetNameOverride: "peer0.icdd",
+}
+
+// peer0.astri.org
+var peer0_astri = golang.Node{
+
+	Addr:      "localhost:7051",
+	TLSCARoot: "/home/davidliu/delphi-fabric/config/ca-crypto-config/peerOrganizations/astri.org/peers/peer0.astri.org/tls/ca.crt",
+
+	SslTargetNameOverride: "peer0.astri.org",
+}
+
+// orderer0.hyperledger
+var orderer0 = golang.Node{
+
+	Addr:      "localhost:7050",
+	TLSCARoot: "/home/davidliu/delphi-fabric/config/ca-crypto-config/ordererOrganizations/hyperledger/orderers/orderer0.hyperledger/tls/ca.crt",
+
+	SslTargetNameOverride: "orderer0.hyperledger",
+}
+
+func nodeRebuild(node golang.Node) basic.Node {
+	return basic.Node{
+		Addr:                  node.Addr,
+		SslTargetNameOverride: node.SslTargetNameOverride,
+		TLSCARoot:             node.TLSCARoot,
+	}
+}
+
+func cryptoRebuild(crypto golang.Crypto) basic.CryptoImpl {
+	return basic.CryptoImpl{
+		Creator:  crypto.Creator,
+		PrivKey:  crypto.PrivKey,
+		SignCert: crypto.SignCert,
+	}
+}
+
+var config = basic.Config{
+	//peer0_icdd
+	Endorsers:  []basic.Node{nodeRebuild(peer0_icdd)},
+	Committers: nil,
+	Orderer:    nodeRebuild(orderer0),
+	Channel:    "allchannel",
+	Chaincode:  "diagnose",
+	Args:       []string{"whoami"},
+}
+var cryptoConfig = golang.CryptoConfig{
+	MSPID:    "astriMSP",
+	PrivKey:  golang.FindKeyFilesOrPanic("/home/davidliu/delphi-fabric/config/ca-crypto-config/peerOrganizations/astri.org/users/Admin@astri.org/msp/keystore/")[0],
+	SignCert: "/home/davidliu/delphi-fabric/config/ca-crypto-config/peerOrganizations/astri.org/users/Admin@astri.org/msp/signcerts/Admin@astri.org-cert.pem",
+	// TODO why not support ~
+}
+
+func TestPing(t *testing.T) {
+	_, err := basic.DialConnection(nodeRebuild(peer0_icdd), logger)
+	goutils.PanicError(err)
+	_, err = basic.DialConnection(nodeRebuild(peer0_astri), logger)
+	goutils.PanicError(err)
+	_, err = basic.DialConnection(nodeRebuild(orderer0), logger)
+	goutils.PanicError(err)
+}
+func TestE2E(t *testing.T) {
+	var err error
+	var proposal *peer.Proposal
+	var signed *peer.SignedProposal
+	var endorser peer.EndorserClient
+	var proposalResponse *peer.ProposalResponse
+	var proposalResponses []*peer.ProposalResponse
+	var transaction *common.Envelope
+	var txResult *orderer.BroadcastResponse
+	var peer0, peer1, ordererGrpc *grpc.ClientConn
+	var ctx = context.Background()
+	defer func() {
+		err = peer0.Close()
+		err = peer1.Close()
+		err = ordererGrpc.Close()
+	}()
+	// client side
+	signer, err := golang.LoadCryptoFrom(cryptoConfig)
+	goutils.PanicError(err)
+	// client side end
+	proposal, txid, err := golang.CreateProposal(
+		signer.Creator,
+		config.Channel,
+		config.Chaincode,
+		"",
+		nil,
+		config.Args...,
+	)
+	goutils.PanicError(err)
+	// server side end
+	var tapeSigner = cryptoRebuild(*signer)
+	signed, err = tapeEngine.SignProposal(proposal, &tapeSigner)
+	goutils.PanicError(err)
+	// server side
+	// peer0.icdd
+	peer0, err = peer0_icdd.AsGRPCClient()
+	goutils.PanicError(err)
+	peer1, err = peer0_astri.AsGRPCClient()
+	endorser = golang.EndorserFrom(peer0)
+	goutils.PanicError(err)
+
+	proposalResponse, err = endorser.ProcessProposal(ctx, signed)
+	goutils.PanicError(err)
+
+	if proposalResponse.Response.Status < 200 || proposalResponse.Response.Status >= 400 {
+		err = fmt.Errorf("Err processing proposal: %s, status: %d, message: %s,\n", err, proposalResponse.Response.Status, proposalResponse.Response.Message)
+		goutils.PanicError(err)
+	}
+
+	utter.Dump(string(proposalResponse.Response.Payload))
+
+	proposalResponses = []*peer.ProposalResponse{proposalResponse}
+	// server side end
+	transaction, err = tapeEngine.CreateSignedTx(proposal, &tapeSigner, proposalResponses)
+	goutils.PanicError(err)
+	// server side
+	ordererGrpc, err = orderer0.AsGRPCClient()
+	goutils.PanicError(err)
+	var committer = golang.Committer{
+		AtomicBroadcastClient: golang.CommitterFrom(ordererGrpc),
+	}
+	err = committer.Setup()
+	goutils.PanicError(err)
+
+	txResult, err = committer.SendRecv(transaction)
+	goutils.PanicError(err)
+	if txResult.Status != common.Status_SUCCESS {
+		panic(txResult)
+	}
+	// server side end
+	utter.Dump(txid)
+
+	var eventer = golang.EventerFrom(ctx, peer1)
+	var seek = eventer.AsTransactionListener(txid)
+	signedEvent, err := seek.SignBy(config.Channel, &tapeSigner)
+	goutils.PanicError(err)
+	_, err = eventer.SendRecv(signedEvent)
+	goutils.PanicError(err)
+	utter.Dump(eventer.ReceiptData)
+}
